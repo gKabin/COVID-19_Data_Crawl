724a1dc76064f9a00ebfe979041da79a578311ff
gKabin <42990748+gKabin@users.noreply.github.com>
2020-04-27 20:09:47 +0545

    Update biorxiv_org.py

--- biorxiv_org.py
+++ biorxiv_org.py
@@ -3,9 +3,10 @@ from requests import get
 from bs4 import BeautifulSoup
 import pandas as pd
 import numpy as np
+import re
+import time
 
-from time import sleep
-from random import randint
+start_time = time.time()
 
 headers = {"Accept-Language": "en-US,en;q=0.5"}
 
@@ -14,25 +15,30 @@ list_authors = []
 list_doi = []
 list_dates = []
 list_links = []
-list_abstracts = [] 
+list_abstract = []
 
-pages = np.arange(1, 50)
+# Parsing URL function
+def getAndParseURL(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    return(soup)
+
+pages = np.arange(1, 10)
 
 for page in pages: 
     
-    page = requests.get("https://connect.biorxiv.org/relate/content/181?page=" + str(page) + " ")
+    #Parsing Pages URL
+    main_url = "https://connect.biorxiv.org/relate/content/181?page=" + str(page) + " "
+    soup = getAndParseURL(main_url) 
     
-    soup = BeautifulSoup(page.text, 'html.parser')    
     covid19 = soup.find_all('div', class_='highwire-article-citation highwire-citation-type-highwire-article')
     doi_date = soup.find_all('div', class_ = 'highwire-cite-metadata')
     
-    sleep(randint(2,10))
-    
+    #Getting Article Contents
     article_num = len(covid19)
-
     for n in np.arange(0, article_num):
         
-        # Getting the title
+        #Getting the title
         title = covid19[n].find('span', class_ = 'highwire-cite-title').a.get_text()
         list_titles.append(title)
 
@@ -42,14 +48,12 @@ for page in pages:
         list_doi.append(new_doi_dates[0])
         list_dates.append(new_doi_dates[1])
 
-        # Getting the link of the article
+        #Getting the link of the article
         link = covid19[n].find('a', class_ = 'highwire-cite-linked-title')['href']
         list_links.append(link)
         
-        # Reading the content
-        article = requests.get(link)
-        article_content = article.text
-        soup_data = BeautifulSoup(article_content, 'html.parser')
+        #Parsing Article URL
+        soup_data = getAndParseURL(link)
         covid19_article = soup_data.find_all('div', class_='panel-pane pane-highwire-article-citation')
         covid19_abstract = soup_data.find_all('div', class_='article abstract-view')
 
@@ -57,9 +61,9 @@ for page in pages:
         authors = covid19_article[1].find('span', class_ = 'highwire-citation-authors').get_text()
         list_authors.append(authors)
 
-        #Getting abstracts
-        abstracts = covid19_abstract[0].find('div', class_ = 'section abstract').p.get_text()
-        list_abstracts.append(abstracts)
+        #Getting abstract
+        abstract = covid19_abstract[0].find('div', class_ = 'section abstract').p.get_text()
+        list_abstract.append(abstract)
 
 #df_show_info
 df_show_info = pd.DataFrame(
@@ -68,7 +72,10 @@ df_show_info = pd.DataFrame(
      'DOI': list_doi,
      'Published Date': list_dates,
      'Link': list_links,
-     'Abstracts': list_abstracts
+     'Abstract': list_abstract
     })
+
+end_time = time.time()
+print("--- Execution time is: %.2f seconds ---" % (end_time - start_time))
 
 print(df_show_info)
